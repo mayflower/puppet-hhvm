aa1f1c05dedcd58dc51f1a19776fdefbc455e783
Robin Gloster <robin@loc-com.de>
2014-08-05 17:32:53 +0200

    inifile config instead of augeas

--- manifests/pgsql.pp
+++ manifests/pgsql.pp
@@ -11,30 +11,27 @@
 # See LICENSE file
 #
 class hhvm::pgsql {
+  ensure_packages('libpq5')
+
+  Package['libpq5'] ->
   wget::fetch { 'http://glob.in/pgsql.so':
     destination => '/usr/lib/hhvm/pgsql.so',
     timeout     => 0,
     verbose     => false,
   } ->
-  augeas { 'hhvm-add-hdf':
-    incl      => '/etc/default/hhvm',
-    changes   => [
-      'set .anon/ADDITIONAL_ARGS "\'--config /etc/hhvm/server.hdf\'"',
-    ],
-    load_path => '/usr/share/augeas/lenses/contrib',
-    lens      => 'HHVM.lns',
-    require   => Class['hhvm::augeas'],
-    notify    => Service['hhvm']
+  ini_setting { 'hhvm-add-hdf':
+    path    => '/etc/default/hhvm',
+    section => '',
+    setting => 'ADDITIONAL_ARGS',
+    value   => '\'--config /etc/hhvm/server.hdf\'',
+    notify  => Service['hhvm']
   } ->
-  augeas { 'hhvm-server-ini-extension':
-    incl      => '/etc/hhvm/server.ini',
-    changes   => [
-      'set .anon/hhvm.dynamic_extension_path /usr/lib/hhvm'
-    ],
-    load_path => '/usr/share/augeas/lenses/contrib',
-    lens      => 'HHVM.lns',
-    require   => Class['hhvm::augeas'],
-    notify    => Service['hhvm']
+  ini_setting { 'hhvm-server-ini-extension':
+    path    => '/etc/hhvm/server.ini',
+    section => '',
+    setting => 'hhvm.dynamic_extension_path',
+    value   => '/usr/lib/hhvm',
+    notify  => Service['hhvm']
   } ->
   file { '/etc/hhvm/server.hdf':
     ensure  => present,
